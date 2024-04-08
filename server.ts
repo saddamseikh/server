f189333912f2dcceb22680cbf5d7affe4ea63bd6
saddamseikh <saddamseikh2229@gmail.com>
2024-04-08 11:56:18 +0530

    initial first prokect setup

---
+++ server.ts
@@ -0,0 +1,6 @@
+import { app } from "./app";
+require("dotenv").config();
+// create our server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is connected with port ${process.env.PORT} `);
+});
